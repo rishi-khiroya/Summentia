237bc91e0e24fcdef8bfd447066ba50feddf0092
Rishi <rk1121@ic.ac.uk>
2024-01-15 15:35:47 +0000

    style: Modify title styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,6 @@
 
 </script>
 
-<div class="sticky top-0 p-5 rounded-md">
-    <h1 class="text-white font-bold text-5xl text-center">Summentia</h1>
+<div class="sticky flex flex-row justify-center top-0 p-5">
+    <h1 class="text-5xl text-center font-sans rounded-lg p-8 px-28 bg-slate-200">Summentia</h1>
 </div>
