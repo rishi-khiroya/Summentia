d3cfce9ecbe7815ee09a4f82791a4c68b7ebb9a4
Khiroya Rishi <rishi.khiroya21@imperial.ac.uk>
2024-01-16 20:30:44 +0000

    feat: Implement submitting a URL for a lecture

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,30 +1,41 @@
 import type { Actions } from './$types';
-import { fail } from '@sveltejs/kit';
+import { error, fail } from '@sveltejs/kit';
 
 export const actions = {
 	submit: async ({ request }) => {
 		const form = await request.formData();
 		// const form = Object.fromEntries(await request.formData());
 
-		// console.log(form);
+		console.log(form);
 		// console.log(formData);
 
 		let lecture: File;
 
-		if (form.get("isLectureFile")) {
+		if (form.get('isLectureFile') === "true") {
+
 			// handle file upload
 			let files: File[] = [];
-			const noFiles: number = parseInt(form.get("noLectureFiles").toString());
+
+			const noFiles: number = parseInt((form.get('noLectureFiles') ?? '0').toString());
 			for (let i = 0; i < noFiles; i++) {
 				let file = form.get(`lectureFile${i}`);
 				if (!!file) files.push(file as File);
 			}
 
 			if (files.length) lecture = files[0];
+
 		} else {
+
 			// handle file from url
+			const data: string = (form.get('lectureURL') ?? '').toString();
+
+			let url: URL;
+			try {
+				url = new URL(data);
+			} catch (exception) {
+				error(400, "Invalid URL.")
+			}
+
 		}
-
-
 	}
 } satisfies Actions;
