98385413bc96aad2e36e34acfa5794865d755c65
Khiroya Rishi <rishi.khiroya21@imperial.ac.uk>
2024-01-16 19:38:24 +0000

    feat: Modify background colour

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,22 @@
 <script>
-    import "../app.css";
+	import '../app.css';
 </script>
   
-<div class="w-screen h-screen">
+<div
+	class="flex flex-col bg-gradient-to-br
+        from-white via-slate-200 to-slate-600
+        dark:from-black dark:via-slate-800 dark:to-slate-700
+        bg-animate min-h-screen h-max justify-between"
+>
     <slot />
 </div>
+
+<style>
+	.bg-animate {
+		background-size: 400%;
+
+		-webkit-animation: AnimationName 10s ease infinite;
+		-moz-animation: AnimationName 10s ease infinite;
+		animation: AnimationName 10s ease infinite;
+	}
+</style>
