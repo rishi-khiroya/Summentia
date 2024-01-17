49e5fe4b263a90ab1ae2e7338c0d35857e43dead
Khiroya Rishi <rishi.khiroya21@imperial.ac.uk>
2024-01-17 23:56:07 +0000

    feat: Roughly implement storing to BLOB.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,6 @@
 import type { Actions } from './$types';
-import { error, fail } from '@sveltejs/kit';
+import { error } from '@sveltejs/kit';
+import { put } from "@vercel/blob";
 
 export const actions = {
 	submit: async ({ request }) => {
@@ -9,20 +10,37 @@ export const actions = {
 		console.log(form);
 		// console.log(formData);
 
-		let lecture: File;
+		let lectureURL: URL;
 
 		if (form.get('isLectureFile') === "true") {
 
 			// handle file upload
-			let files: File[] = [];
-
+			const urls: URL[] = [];
 			const noFiles: number = parseInt((form.get('noLectureFiles') ?? '0').toString());
+
+			if (noFiles != 1) console.warn(`${noFiles} lecture files have been uploaded. This could be a mistake.`);
+
 			for (let i = 0; i < noFiles; i++) {
-				let file = form.get(`lectureFile${i}`);
-				if (!!file) files.push(file as File);
+				const file: File = form.get(`lectureFile${i}`) as File;
+				if (file) {
+					console.log(file);
+					const { url: urlPath } = await put(file.name, file, { access: "public" });
+
+					let url;
+					try {
+						url = new URL(urlPath);
+						urls
+					} catch (_) {
+						error(400, "Error accessing uploaded file.");
+					}
+
+					urls.push(url);
+				}
 			}
 
-			if (files.length) lecture = files[0];
+			if (urls.length != 1) console.warn(`${urls.length} lecture files have been uploaded succesfully to BLOB. There should only have been 1. Continuing using the first URL (${urls[0]})`);
+
+			lectureURL = urls[0];
 
 		} else {
 
@@ -32,10 +50,16 @@ export const actions = {
 			let url: URL;
 			try {
 				url = new URL(data);
-			} catch (exception) {
+			} catch (_) {
 				error(400, "Invalid URL.")
 			}
 
+			console.log(url.hostname);
+			lectureURL = url;
+
 		}
+
+		console.log(`Final lectureURL=${lectureURL}`);
+
 	}
 } satisfies Actions;
