fbb3ec3229b3db677b5767549274c574431fca6b
Dami <dme21@ic.ac.uk>
2024-01-17 17:09:50 +0000

    feat: Implement audio extraction

--- test.ts
+++ test.ts
@@ -1,5 +1,48 @@
 import * as fs from 'fs';
+import ffmpegStatic from 'ffmpeg-static';
+import ffmpeg from 'fluent-ffmpeg';
+import { Stream } from 'stream';
 
+async function extract_audio() {
+    let data;
+
+    const outStream = new Stream.Writable({
+        write(chunk, encoding, next) {
+            // console.log(chunk.toString())
+            next()
+        }
+    });
+
+    const inStream = new Stream.Readable({
+        read() {}
+    });
+
+    ffmpeg.setFfmpegPath(ffmpegStatic);
+
+    ffmpeg()
+        .input("./src/static/door_tier_list.mp4")
+        .outputOptions('-ab', '192k')
+        .on('progress', (progress) => {
+            if (progress.percent) {
+                console.log(`Processing: ${Math.floor(progress.percent)}% done`);
+            }
+        })
+        .on('end', () => {
+            console.log('FFmpeg has finished.');
+        })
+        .on('error', (error) => {
+            console.error(error);
+        })
+        .format('wav')
+        .save("./src/static/door_tier_list.wav")
+        // .stream(outStream);
+
+    inStream.pipe(outStream);
+    inStream.on('data', (d) => {
+        data = d;
+    })
+    return data;
+}
 async function query(filename) {
 	const data = fs.readFileSync(filename);
 	const response = await fetch(
@@ -14,6 +57,11 @@ async function query(filename) {
 	return result;
 }
 
-query("sample1.flac").then((response) => {
-	console.log(JSON.stringify(response));
-});
+try {
+    await extract_audio();
+    query("sample1.flac").then((response) => {
+        console.log(JSON.stringify(response));
+    });
+} catch (error) {
+    console.log(error);
+}
