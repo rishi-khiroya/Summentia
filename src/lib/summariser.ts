df1ff7352892dd03c7a8f68b0f16e048e352cd8a
BrettConway2 <brett@conwayclan.com>
2024-01-17 12:26:15 +0000

    fix: Fix Summariser argument type
    Change Summariser argument type from string to String

--- src/lib/summariser.ts
+++ src/lib/summariser.ts
@@ -5,12 +5,12 @@ export class Summariser{
     public constructor(){
     }
 
-    public async summarise(transcript: string) {
+    public async summarise(transcript: String) {
         const completion = await openai.chat.completions.create({
           messages: [{ role: "system", content: "Please can you summarise this: " + transcript}],
           model: "gpt-3.5-turbo",
         });
-        
+
         return(completion.choices[0]["message"]["content"]);
       }
 }
