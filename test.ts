23edc656d488e9e78d78711efb26149976f31a67
Dami <dme21@ic.ac.uk>
2024-01-17 17:32:02 +0000

    feat: Attempt to use API to transcribe audio
    inStream may be broken, and async execution order means audio isn't used by API.

--- test.ts
+++ test.ts
@@ -2,6 +2,7 @@ import * as fs from 'fs';
 import ffmpegStatic from 'ffmpeg-static';
 import ffmpeg from 'fluent-ffmpeg';
 import { Stream } from 'stream';
+// import * as https from 'node:https';
 
 async function extract_audio() {
     let data;
@@ -43,25 +44,38 @@ async function extract_audio() {
     })
     return data;
 }
-async function query(filename) {
-	const data = fs.readFileSync(filename);
-	const response = await fetch(
-		"https://api-inference.huggingface.co/models/openai/whisper-large-v3",
-		{
-			headers: { Authorization: "Bearer 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" },
-			method: "POST",
-			body: data,
-		}
-	);
-	const result = await response.json();
-	return result;
+
+// const testVid: string = "https://y7ceexhkahxpnqji.public.blob.vercel-storage.com/test.mp4";
+// const url: URL = new URL(testVid);
+
+async function run_query(data) {
+    // let data;
+    // https.get(filename, (res) => {
+    //     res.on('data', (d) => {
+    //         // console.log(data);
+    //         // console.log(JSON.parse(JSON.stringify(d)));
+    //         // data = JSON.parse(JSON.stringify(d))['data'];
+    //     });
+    // });
+
+    const response = await fetch(
+        "https://api-inference.huggingface.co/models/openai/whisper-large-v3",
+        {
+            headers: { Authorization: "Bearer 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" },
+            method: "POST",
+            body: data,
+        }
+    );
+    console.log(response);
+    // const result = await response.json();
+    // console.log(result);
+    // return result;
 }
 
+let data;
 try {
-    await extract_audio();
-    query("sample1.flac").then((response) => {
-        console.log(JSON.stringify(response));
-    });
+    data = await extract_audio();
+    await run_query(data);
 } catch (error) {
     console.log(error);
 }
