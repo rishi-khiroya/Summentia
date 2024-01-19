4b2acb5c768f447004631d8db70dc203fc702076
Khiroya Rishi <rishi.khiroya21@imperial.ac.uk>
2024-01-19 17:02:40 +0000

    feat: Implement uploading uploaded files to BLOB.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,48 +1,59 @@
 import type { Actions } from './$types';
 import { error } from '@sveltejs/kit';
 import { put } from "@vercel/blob";
+// import { writeFile } from 'fs/promises';
+
+import { BLOB_READ_WRITE_TOKEN } from '$env/static/private'
 
 export const actions = {
 	submit: async ({ request }) => {
 		const form = await request.formData();
-		// const form = Object.fromEntries(await request.formData());
-
-		console.log(form);
-		// console.log(formData);
-
 		let lectureURL: URL;
+
+		const uploadToBlob = async (file: File) => {
+			console.log(`Uploading ${file.name} to BLOB store.`)
+			const { url: urlPath } = await put(file.name, file, { access: "public", token: BLOB_READ_WRITE_TOKEN });
+
+			let url;
+			try {
+				url = new URL(urlPath);
+			} catch (_) {
+				error(400, "Error accessing uploaded file.");
+			}
+			console.log(`Successfully uploaded ${file.name} to BLOB store.`)
+
+			lectureURL = url;
+		}	
+
+		const process = async (file: File) => {
+			// TODO: implement pipeline
+			// const transcript: string = await transcribe(...);
+			// const lecture: Lecture = new Lecture(...);
+			// const summary: Sumamry = summarise(lecture);
+			console.log("process: ", file.name)
+			return;
+		}
 
 		if (form.get('isLectureFile') === "true") {
 
 			// handle file upload
-			const urls: URL[] = [];
-			const noFiles: number = parseInt((form.get('noLectureFiles') ?? '0').toString());
+			const file: File = form.get(`lectureFile`) as File;
+			console.log(file);
 
-			if (noFiles != 1) console.warn(`${noFiles} lecture files have been uploaded. This could be a mistake.`);
+			// TODO: write file to some temp dir
+			// await writeFile(`./files/${file.name}`,new Uint8Array(await  file.arrayBuffer()));		
+			// console.log("file written to: ", file.name);	
 
-			for (let i = 0; i < noFiles; i++) {
-				const file: File = form.get(`lectureFile${i}`) as File;
-				if (file) {
-					console.log(file);
-					const { url: urlPath } = await put(file.name, file, { access: "public" });
-
-					let url;
-					try {
-						url = new URL(urlPath);
-						urls
-					} catch (_) {
-						error(400, "Error accessing uploaded file.");
-					}
-
-					urls.push(url);
-				}
+			if (file) {
+				const uploadTask: Promise<void> = uploadToBlob(file);
+				const processTask: Promise<void> = process(file);
+				
+				// wait for async tasks to finish
+				Promise.all([uploadTask, processTask]);
 			}
 
-			if (urls.length != 1) console.warn(`${urls.length} lecture files have been uploaded succesfully to BLOB. There should only have been 1. Continuing using the first URL (${urls[0]})`);
-
-			lectureURL = urls[0];
-
 		} else {
+			// TODO: not priorty - stretch goal
 
 			// handle file from url
 			const data: string = (form.get('lectureURL') ?? '').toString();
@@ -54,12 +65,13 @@ export const actions = {
 				error(400, "Invalid URL.")
 			}
 
-			console.log(url.hostname);
 			lectureURL = url;
+			console.log(lectureURL);
+
+			const file: File | null = null;
+			if (file) await process(file);
 
 		}
-
-		console.log(`Final lectureURL=${lectureURL}`);
 
 	}
 } satisfies Actions;
