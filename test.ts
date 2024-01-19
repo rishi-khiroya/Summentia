1fc83dd2be10671b8e601b6162b35b4d24f4f296
Dami <dme21@ic.ac.uk>
2024-01-19 14:10:50 +0000

    style: Remove comments and extra whitespace in test.ts

--- test.ts
+++ test.ts
@@ -1,23 +1,8 @@
 import * as fs from 'fs';
 import ffmpegStatic from 'ffmpeg-static';
 import ffmpeg from 'fluent-ffmpeg';
-// import { Stream } from 'stream';
-// import * as https from 'node:https';
 
 async function extract_audio(filename: string) {
-    // let data;
-
-    // const outStream = new Stream.Writable({
-    //     write(chunk, encoding, next) {
-    //         // console.log(chunk.toString())
-    //         next()
-    //     }
-    // });
-
-    // const inStream = new Stream.Readable({
-    //     read() {}
-    // });
-
     ffmpeg.setFfmpegPath(ffmpegStatic);
 
     ffmpeg()
@@ -36,28 +21,9 @@ async function extract_audio(filename: string) {
         })
         .format('mp3')
         .save("./src/static/" + filename + ".mp3")
-        // .stream(outStream);
-
-    // inStream.pipe(outStream);
-    // inStream.on('data', (d) => {
-    //     data = d;
-    // })
-    // return data;
 }
 
-// const testVid: string = "https://y7ceexhkahxpnqji.public.blob.vercel-storage.com/test.mp4";
-// const url: URL = new URL(testVid);
-
 async function run_query(filename) {
-    // let data;
-    // https.get(filename, (res) => {
-    //     res.on('data', (d) => {
-    //         // console.log(data);
-    //         // console.log(JSON.parse(JSON.stringify(d)));
-    //         // data = JSON.parse(JSON.stringify(d))['data'];
-    //     });
-    // });
-
     const data = fs.readFileSync("./src/static/" + filename + ".mp3");
     console.log(data);
     const response = await fetch(
@@ -68,14 +34,11 @@ async function run_query(filename) {
             body: data,
         }
     );
-    // console.log(response);
     const result = await response.json();
     console.log(result);
-    // return result;
 }
 
 try {
-    // TODO: extract name so simpler
     const file = "door_tier_list";
     extract_audio(file);
     console.log("EXTRACTION COMPLETE");
