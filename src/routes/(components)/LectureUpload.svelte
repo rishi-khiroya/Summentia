cff957b0fd8be654cad79d50b3045ae55526b655
Khiroya Rishi <rishi.khiroya21@imperial.ac.uk>
2024-01-19 17:01:46 +0000

    fix: Fix LectureUpload to allow click and upload

--- src/routes/(components)/LectureUpload.svelte
+++ src/routes/(components)/LectureUpload.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
 	import { Dropzone, Input, Toggle } from 'flowbite-svelte';
 
-	export let lectureFile: File[];
-    export let doLectureUpload: boolean;
-    export let lectureURL: string;
+	let lectureFile: File[] = [];
+	export let doLectureUpload: boolean;
+	export let lectureURL: string;
+
+	export let fileList: FileList;
 
 	const dropHandle = (event) => {
 		lectureFile = [];
 		event.preventDefault();
 		if (event.dataTransfer.items) {
-			[...event.dataTransfer.items].forEach((item, i) => {
+			[...event.dataTransfer.items].forEach((item) => {
 				if (item.kind === 'file') {
 					const file = item.getAsFile();
 					lectureFile.push(file.name);
@@ -17,7 +19,7 @@
 				}
 			});
 		} else {
-			[...event.dataTransfer.files].forEach((file, i) => {
+			[...event.dataTransfer.files].forEach((file) => {
 				lectureFile = file.name;
 			});
 		}
@@ -29,6 +31,7 @@
 			lectureFile.push(files[0].name);
 			lectureFile = lectureFile;
 		}
+		fileList = files;
 	};
 
 	const showFiles = (files: Blob[]) => {
@@ -81,15 +84,13 @@
 				<p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
 					<span class="font-semibold">Click to upload</span> or drag and drop
 				</p>
-				<p class="text-xs text-gray-500 dark:text-gray-400">
-					SVG, PNG, JPG or GIF (MAX. 800x400px)
-				</p>
+				<p class="text-xs text-gray-500 dark:text-gray-400">.mp4 files only</p>
 			{:else}
 				<p>{showFiles(lectureFile)}</p>
 			{/if}
 		</Dropzone>
 	{:else}
 		<h1 class="text-lg">Upload from URL:</h1>
-		<Input type="url" placeholder="Enter lecture URL" bind:value={lectureURL}/>
+		<Input type="url" placeholder="Enter lecture URL" bind:value={lectureURL} />
 	{/if}
 </div>
