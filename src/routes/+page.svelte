51471fb4adfd5d8109f81b92450300b41f197627
Bjorna Permeti <bp821@ic.ac.uk>
2024-01-21 17:17:24 +0000

    feat: Added Slide Uploading in the frontend

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
 	import { Button } from 'flowbite-svelte';
-	import MainAccordion from './(components)/MainAccordion.svelte';
+	import { AccordionItem, Accordion } from 'flowbite-svelte';
+    import LectureUpload from './(components)/LectureUpload.svelte';
+	import { fromJSON } from 'postcss';
+	import SlidesUpload from './(components)/SlidesUpload.svelte';
 
-	let lectureFile: File[] = [];
+	let lectureFile:File = [];
 	let doLectureUpload = false;
 	let lectureURL: string;
+	let slidesFile: File[] = [];
 
 	async function submit() {
 		const form = new FormData();
@@ -21,13 +25,33 @@
 			method: 'POST',
 			body: form
 		});
+		form.append('slidesFile', slidesFile);
 	}
 </script>
 
 <div class="flex-1 p-5">
 	
-	<MainAccordion />
-
+	<div class="flex-1">
+		<Accordion>
+		  <AccordionItem>
+			<span slot="header">Upload Lecture</span>
+			<LectureUpload bind:lectureFile bind:doLectureUpload bind:lectureURL />
+		  </AccordionItem>
+		  <AccordionItem>
+			<span slot="header">Upload Slides</span>
+			<SlidesUpload bind:slidesFile />
+		  </AccordionItem>
+		
+		  <AccordionItem>
+			<span slot="header">Supplementary Info</span>
+			
+		  </AccordionItem>
+		  <AccordionItem>
+			<span slot="header">Customisation</span>
+			
+		  </AccordionItem>
+		</Accordion>
+		</div>
 	<Button type="submit" on:click={() => submit()} color="dark" class="text-white m-5 p-4"
 		>Submit</Button
 	>
