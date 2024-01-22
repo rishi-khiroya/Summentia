643d0c15b5ac790b0ab19dfd8fe5bcac3c6be250
Gabriel de Olim Gaul <gabdeolimgaul@outlook.com>
2024-01-22 13:13:55 +0000

    build: Update config files to use sveltekit-python-vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { sveltekit_python_vercel } from "sveltekit-python-vercel/vite";
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit_python_vercel(), sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
