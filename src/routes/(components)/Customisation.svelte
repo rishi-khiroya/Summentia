8afc0e90a7fd04d4e57886f8f5591695f0a9cf2a
bjorna <bjornapermeti@gmail.com>
2024-01-23 23:04:02 +0000

    feat: Added Customisation to front end

---
+++ src/routes/(components)/Customisation.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import { Input, Label, Checkbox, Radio } from 'flowbite-svelte';
+
+	export let nrOfPages: number;
+	export let areKeyWordsHighlighted: boolean;
+	export let typeOfSummary: string;
+	export let hasQandAns: boolean;
+</script>
+
+<div class="customization">
+	<div>
+		<Label for="first_name" class="mb-2">Enter max number of pages for the summary</Label>
+		<Input
+			type="number"
+			id="nr-pages"
+			placeholder="Max Number of Pages"
+			required
+			bind:value={nrOfPages}
+		/>
+	</div>
+	<Checkbox checked={areKeyWordsHighlighted}>Highlight Key Words</Checkbox>
+	<div>
+		<Label for="summary-types" class="mb-2">Types of Summary:</Label>
+		<Radio name="type" value={"normal text format"} bind:group={typeOfSummary}>Text Format</Radio>
+		<Radio name="type" value={"bullet points"} bind:group={typeOfSummary}>Bullet Points</Radio>
+	</div>
+    <div>
+        <Label for="extra-features" class="mb-2">Extra Features:</Label>
+        <Checkbox checked={hasQandAns}>Generate Questions and Answers</Checkbox>
+    </div>
+</div>
+
+<style>
+	.customization {
+		display: flex;
+		flex-direction: column;
+		gap: 1em;
+		height: 100%;
+		perspective: 100vh;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
 	import { Button } from 'flowbite-svelte';
 	import { AccordionItem, Accordion } from 'flowbite-svelte';
-    import LectureUpload from './(components)/LectureUpload.svelte';
-	import { fromJSON } from 'postcss';
-	import SlidesUpload from './(components)/SlidesUpload.svelte';
+	import LectureUpload from './(components)/LectureUpload.svelte';
+    import SlidesUpload from './(components)/SlidesUpload.svelte';
 	import SupplementaryUpload from './(components)/SupplementaryUpload.svelte';
+	import Customisation from './(components)/Customisation.svelte';
 
-	let lectureFile:File[] = [];
+	let lectureFile: File[] = [];
 	let doLectureUpload = false;
 	let lectureURL: string;
 	let slidesFile: File;
 	let supplementaryFiles: File[] = [];
+	let nrOfPages: number;
+	let areKeyWordsHighlighted: boolean;
+	let typeOfSummary: string;
+	let hasQandAns: boolean;
 
 	async function submit() {
 		const form = new FormData();
@@ -29,35 +33,44 @@
 		});
 		form.append('slidesFile', slidesFile);
 		supplementaryFiles.forEach((file, i) => {
-				form.append(`supplementaryFiles${i}`, file);
-			});
+			form.append(`supplementaryFiles${i}`, file);
+		});
+		form.append('nrOfPages', nrOfPages);
+		form.append('areKeyWordsHighlighted', areKeyWordsHighlighted);
+		form.append('typeOfSummary', typeOfSummary);
+		form.append('hasQandAns', hasQandAns);
 	}
 </script>
 
 <div class="flex-1 p-5">
-	
 	<div class="flex-1">
 		<Accordion>
-		  <AccordionItem>
-			<span slot="header">Upload Lecture</span>
-			<LectureUpload bind:lectureFile bind:doLectureUpload bind:lectureURL />
-		  </AccordionItem>
-		  <AccordionItem>
-			<span slot="header">Upload Slides</span>
-			<SlidesUpload bind:slidesFile />
-		  </AccordionItem>
-		
-		  <AccordionItem>
-			<span slot="header">Add Supplementary Info</span>
-			<SupplementaryUpload bind:supplementaryFiles />
-		  </AccordionItem>
-		  <AccordionItem>
-			<span slot="header">Customisation</span>
-			
-		  </AccordionItem>
+			<AccordionItem>
+				<span slot="header">Upload Lecture</span>
+				<LectureUpload bind:lectureFile bind:doLectureUpload bind:lectureURL />
+			</AccordionItem>
+			<AccordionItem>
+				<span slot="header">Upload Slides</span>
+				<SlidesUpload bind:slidesFile />
+			</AccordionItem>
+
+			<AccordionItem>
+				<span slot="header">Add Supplementary Info</span>
+				<SupplementaryUpload bind:supplementaryFiles />
+			</AccordionItem>
+			<AccordionItem>
+				<span slot="header">Customisation</span>
+				<Customisation
+					bind:nrOfPages
+					bind:areKeyWordsHighlighted
+					bind:typeOfSummary
+					bind:hasQandAns
+				/>
+			</AccordionItem>
 		</Accordion>
-		</div>
+	</div>
+	<div style="display: flex; justify-content: flex-end;">
 	<Button type="submit" on:click={() => submit()} color="dark" class="text-white m-5 p-4"
-		>Submit</Button
-	>
+		>Submit</Button>
+	</div>
 </div>
