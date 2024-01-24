bcaac9b30b06584482db33796b41611192945567
bjorna <bjornapermeti@gmail.com>
2024-01-24 01:04:18 +0000

    feat: Generate and download latex file given the latex code in a string as a parameter

--- src/lib/output_engine.ts
+++ src/lib/output_engine.ts
@@ -6,5 +6,19 @@ function generateLatexFile(latexCode: string): void {
     fs.writeFileSync(fileName, latexCode, 'utf-8');
 }
 
+function generateAndDownloadLatex(latexCode: string): void {
+    
+    // Create a Blob with the LaTeX content
+    const blob = new Blob([latexCode], { type: 'application/x-latex' });
 
+    // Create a link element
+    const link = document.createElement('a');
+    link.download = fileName;
+    link.href = window.URL.createObjectURL(blob);
+    document.body.appendChild(link);
 
+    // Trigger a click event on the link to start the download
+    link.click();
+
+    document.body.removeChild(link);
+}
