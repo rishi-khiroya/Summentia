aa94da512a3f9d76194ad55b045a740d3664349f
BrettConway2 <brett@conwayclan.com>
2024-01-28 23:02:28 +0000

    feat: Add Customisation interface
    To be integrated into refactorising of the summariser

---
+++ src/lib/types/Customisation.ts
@@ -0,0 +1,8 @@
+interface Customisation{
+    latex_flag: boolean;           // default false
+    highlight_keywords: boolean;   // default false
+    questions: boolean;            // default false
+    summary_format: string;        // default ""
+    length: number;                // default -1
+    detail_level: number;          // default 1
+}
