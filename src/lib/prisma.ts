3ea14682531e961198b6c3c2a1bac02855fd32ee
Khiroya Rishi <rishi.khiroya21@imperial.ac.uk>
2024-01-30 11:38:35 +0000

    feat: Roughly implement basic DB functions.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,22 @@
-import { PrismaClient } from '@prisma/client';
- 
-export const prisma = new PrismaClient();
+import { PrismaClient, type Project, type User } from '@prisma/client';
+import type { PrismaProject, PrismaUser } from './types/prisma';
+
+const prisma = new PrismaClient();
+
+export async function createUser(data: PrismaUser): Promise<User> {
+    // TODO: VERIFY EMAIL 
+    return await prisma.user.create({ data })
+}
+
+export async function createProject(data: PrismaProject): Promise<Project> {
+    return await prisma.project.create({ data });
+}
+
+export async function getProjectsByUserId(id: number): Promise<Project[]> {
+    const result = await prisma.user.findUnique({
+        where: { id },
+        select: { projects: true }
+    });
+
+    return result ? result.projects : [];
+}
