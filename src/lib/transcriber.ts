ab79416878139185971505e6b1485b48d73ef521
Dami <dme21@ic.ac.uk>
2024-02-05 17:16:46 +0000

    feat: Change extract_audio to be an async function

--- src/lib/transcriber.ts
+++ src/lib/transcriber.ts
@@ -8,34 +8,32 @@ import { openai } from './openai_clinet';
 
 const { setFfmpegPath } = pkg;
 
-function extract_audio(
-	filePath: string,
-	resolve: { (value: void | PromiseLike<void>): void; (): void },
-	reject: (reason?: unknown) => void
-): void {
+async function extract_audio(filePath: string): Promise<void> {
 	if (!ffmpegPath) {
-		reject("ffmpegPath cannot be found in ffmpeg-static package");
+		throw new Error(' cannot be found in ffmpeg-static package');
 	} else {
 		setFfmpegPath(ffmpegPath);
-		ffmpeg()
-			.input(createReadStream(filePath + '.mp4'))
-			.outputOptions('-ab', '96k')
-			.on('progress', (progress) => {
-				if (progress.percent) {
-					console.log(`Processing: ${Math.floor(progress.percent)}% done`);
-				}
-			})
-			.on('end', () => {
-				console.log('FFmpeg has finished.');
-				resolve();
-			})
-			.on('error', (error) => {
-				reject(error);
-			})
-			.format('mp3')
-			.save(filePath + '.mp3');
+
+		await new Promise<void>((resolve, reject) => {
+			ffmpeg()
+				.input(createReadStream(filePath + '.mp4'))
+				.outputOptions('-ab', '96k')
+				.on('progress', (progress) => {
+					if (progress.percent) {
+						console.log(`Processing: ${Math.floor(progress.percent)}% done`);
+					}
+				})
+				.on('end', () => {
+					console.log('FFmpeg has finished.');
+					resolve();
+				})
+				.on('error', (error) => {
+					reject(error);
+				})
+				.format('mp3')
+				.save(filePath + '.mp3');
+		});
 	}
-	// return true;
 }
 
 async function get_transcription(filePath: string) {
@@ -47,9 +45,7 @@ async function get_transcription(filePath: string) {
 
 export async function transcribe(filePath: string): Promise<string | null> {
 	try {
-		await new Promise<void>((resolve, reject) => {
-			extract_audio(filePath, resolve, reject);
-		});
+		await extract_audio(filePath);
 		console.log('FFMPEG is done processing.');
 		const transcription = await get_transcription(filePath);
 		console.log('transcription complete');
