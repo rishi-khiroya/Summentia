400f4ae36b39664dad995c4bb2a6c6c889b680db
Gabriel de Olim Gaul <gabdeolimgaul@outlook.com>
2024-02-12 13:17:37 +0000

    style: Fix indenting with two spaces instead of four to maintain consistency

--- src/lib/formatter.ts
+++ src/lib/formatter.ts
@@ -6,41 +6,41 @@ import type {Customisation} from "$lib/types/Customisation";
 const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
 
 // we assumed that transcript_code is a string of LaTeX code containing the summary
-export async function format(transcript_code: string, customisations: Customisation){
-
-  let highlightPhrase = "";
-  if (customisations.highlightKeywords){
-    highlightPhrase = " highlight keywords ";
-  }
+export async function format(transcript_code: string, customisations: Customisation) {
+    
+    let highlightPhrase = "";
+    if (customisations.highlightKeywords) {
+        highlightPhrase = " highlight keywords ";
+    }
   
-  let questionsPhrase = "";
-  if (customisations.questions){
-    questionsPhrase = " with a revision question answer section"
-  }
+    let questionsPhrase = "";
+    if (customisations.questions) {
+        questionsPhrase = " with a revision question answer section"
+    }
 
-  // adds length prompt for length upper bound in pages, default set to 1
-  let lengthPhrase = "";
-  if (customisations.length != -1){
-    if (customisations.length == 1){
-      lengthPhrase = " in 1 page ";
-    } else {
-      lengthPhrase = " in " + customisations.length + " pages ";
-    } 
-  }
+    // adds length prompt for length upper bound in pages, default set to 1
+    let lengthPhrase = "";
+    if (customisations.length != -1) {
+        if (customisations.length == 1) {
+            lengthPhrase = " in 1 page ";
+        } else {
+            lengthPhrase = " in " + customisations.length + " pages ";
+        } 
+    }
   
-  const prompt = "Plase give me this LaTeX code, " + lengthPhrase + highlightPhrase + customisations.summaryFormat + questionsPhrase +" :" + transcript_code;
+    const prompt = "Plase give me this LaTeX code, " + lengthPhrase + highlightPhrase + customisations.summaryFormat + questionsPhrase +" :" + transcript_code;
 
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: prompt}],
-      model: "gpt-3.5-turbo",
-  });
+    const completion = await openai.chat.completions.create({
+        messages: [{ role: "system", content: prompt}],
+        model: "gpt-3.5-turbo",
+    });
 
-  const summary = completion.choices[0]["message"]["content"];
+    const summary = completion.choices[0]["message"]["content"];
 
-  console.log("Prompt : ");
-  console.log(prompt);
-  console.log("Customised Code : ");
-  console.log(summary);
-        
-  return summary;
+    console.log("Prompt : ");
+    console.log(prompt);
+    console.log("Customised Code : ");
+    console.log(summary);
+            
+    return summary;
 }
--- src/lib/summariser.ts
+++ src/lib/summariser.ts
@@ -3,38 +3,38 @@ import OpenAI from 'openai';
 
 const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
 
-export async function summarise(transcript: string, detailLevel: number = 1){
+export async function summarise(transcript: string, detailLevel: number = 1) {
 
-  // Adds prompt the level parameter for the extent of a given summary
-  let detailPhrase = "";
-  switch (detailLevel){
-    case 0:
-      detailPhrase = " in 3 sentences or less: ";
-      break;
-    case 1:
-      detailPhrase = ": ";
-      break;
-    case 2:
-      detailPhrase = " in great detail: ";
-      break;
-    default:
-      break;
-  }
+    // Adds prompt the level parameter for the extent of a given summary
+    let detailPhrase = "";
+    switch (detailLevel) {
+        case 0:
+            detailPhrase = " in 3 sentences or less: ";
+            break;
+        case 1:
+            detailPhrase = ": ";
+            break;
+        case 2:
+            detailPhrase = " in great detail: ";
+            break;
+        default:
+            break;
+    }
 
-  const prompt = "Please summarise this" +  detailPhrase + transcript;
-  
+    const prompt = "Please summarise this" +  detailPhrase + transcript;
+    
 
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: prompt}],
-      model: "gpt-3.5-turbo",
-  });
+    const completion = await openai.chat.completions.create({
+        messages: [{ role: "system", content: prompt}],
+        model: "gpt-3.5-turbo",
+    });
 
-  const summary = completion.choices[0]["message"]["content"];
+    const summary = completion.choices[0]["message"]["content"];
 
-	console.log('Prompt : ');
-	console.log(prompt);
-	console.log('Summary : ');
-	console.log(summary);
+    console.log('Prompt : ');
+    console.log(prompt);
+    console.log('Summary : ');
+    console.log(summary);
 
-	return summary;
+    return summary;
 }
