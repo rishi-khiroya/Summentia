e4919e85944711ecf19e90fab704a70f10f16a82
Khiroya Rishi <rishi.khiroya21@imperial.ac.uk>
2024-02-15 02:52:11 +0000

    feat: Add blank settings page

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -1,6 +1,18 @@
 <script>
 	import { page } from '$app/stores';
-	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Button, Dropdown, DropdownDivider, DropdownItem, Avatar, DropdownHeader } from 'flowbite-svelte';
+	import {
+		Navbar,
+		NavBrand,
+		NavLi,
+		NavUl,
+		NavHamburger,
+		Button,
+		Dropdown,
+		DropdownDivider,
+		DropdownItem,
+		Avatar,
+		DropdownHeader
+	} from 'flowbite-svelte';
 	import { DarkMode } from 'flowbite-svelte';
 	import { signIn, signOut } from '@auth/sveltekit/client';
 </script>
@@ -21,7 +33,11 @@
 			<DarkMode class="text-primary-500 dark:text-primary-600 border dark:border-gray-800" />
 			{#if $page.data.session}
 				<div class="flex items-center md:order-2">
-					<Avatar id="avatar-menu" src={$page.data.session.user.image} class="hover:cursor-pointer"/>
+					<Avatar
+						id="avatar-menu"
+						src={$page.data.session.user.image}
+						class="hover:cursor-pointer"
+					/>
 					<NavHamburger class1="w-full md:flex md:w-auto md:order-1" />
 				</div>
 				<Dropdown placement="bottom" triggeredBy="#avatar-menu">
@@ -30,7 +46,7 @@
 					</DropdownHeader>
 					<DropdownItem href="dashboard">Dashboard</DropdownItem>
 					<DropdownItem href="projects">Projects</DropdownItem>
-					<DropdownItem>Settings</DropdownItem>
+					<DropdownItem href="settings">Settings</DropdownItem>
 					<DropdownDivider />
 					<DropdownItem on:click={() => signOut()}>Sign out</DropdownItem>
 				</Dropdown>
@@ -38,14 +54,14 @@
 				<Button
 					outline
 					color="light"
-					on:click={() => signIn("auth0")}
+					on:click={() => signIn('auth0')}
 					class="text-primary-500 dark:text-primary-600 border dark:border-gray-800 dark:hidden"
 					>Sign in</Button
 				>
 				<Button
 					outline
 					color="dark"
-					on:click={() => signIn("auth0")}
+					on:click={() => signIn('auth0')}
 					class="text-primary-500 dark:text-primary-600 border dark:border-gray-800 dark:block hidden"
 					>Sign in</Button
 				>
