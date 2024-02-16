c1c8c419ea7c8a121ca20a6d253e7fecbe706d2b
Gabriel de Olim Gaul <gabdeolimgaul@outlook.com>
2024-02-16 16:00:22 +0000

    refactor: Wrap the download and dropdown button in a button group

--- src/routes/projects/[slug]/+page.svelte
+++ src/routes/projects/[slug]/+page.svelte
@@ -1,24 +1,33 @@
 <script lang="ts">
-    import { Button, Dropdown, DropdownItem, VideoPlaceholder, TextPlaceholder, CardPlaceholder} from 'flowbite-svelte';
     import { EditSolid, DownloadSolid, AngleDownSolid } from 'flowbite-svelte-icons';
     import InformationBox from './InformationBox.svelte'
-
+    import { 
+        ButtonGroup, 
+        Button, 
+        Dropdown, 
+        DropdownItem, 
+        VideoPlaceholder, 
+        TextPlaceholder, 
+        CardPlaceholder
+    } from 'flowbite-svelte';
 </script>
 
 <div class="max-h-screen flex-1">
     <div class="flex-col px-10 pt-10">
         <div class="justify-start items-center flex">
-            <Button class="h-10 w-10" pill={true} color="dark"><EditSolid /></Button>
+            <Button class="h-10 w-10" pill={true} color="dark"><EditSolid class="focus:!outline-none" /></Button>
             <h1 class="text-4xl p-5 font-bold">Intro to IML video</h1>
-            <Button class="z-0" pill={false} color="dark"><DownloadSolid class="me-2"/>Download Summary</Button>
-            <Button class="border-l border-gray-500 w-5 relative -left-3 z-10 focus-within:ring-0 focus:ring-0" pill={false} color="dark">
-                <AngleDownSolid class="focus:!outline-none"></AngleDownSolid>
-            </Button>
-            <Dropdown>
-                <DropdownItem>.pdf</DropdownItem>
-                <DropdownItem>.docx</DropdownItem>
-                <DropdownItem>.txt</DropdownItem>
-            </Dropdown>
+            <ButtonGroup>
+                <Button pill color="dark"><DownloadSolid class="me-2 focus:!outline-none"/>Download Summary</Button>
+                <Button class="border-l border-gray-500 w-5" pill={false} color="dark">
+                    <AngleDownSolid class="focus:!outline-none"></AngleDownSolid>
+                </Button>
+                <Dropdown>
+                    <DropdownItem>.pdf</DropdownItem>
+                    <DropdownItem>.docx</DropdownItem>
+                    <DropdownItem>.txt</DropdownItem>
+                </Dropdown>
+            </ButtonGroup>
         </div>
         <div class="flex">
             <div class="flex-col flex-1">
