988e04f50920b277984b0ab10a866c16af1d82f0
BrettConway2 <brett@conwayclan.com>
2024-02-18 13:32:03 +0000

    feat: Centre the slide picture and summary
    add justify-center and items-center to class around flex boxes

--- src/routes/slide_editor/+page.svelte
+++ src/routes/slide_editor/+page.svelte
@@ -5,11 +5,11 @@
     let summaryText = 'This is the second lecture for the Introduction to Machine Learning course';
 </script>
 
+
 <div class="flex flex-col p-10">
     <h1 class="text-4xl p-10 font-bold">Slide Editor</h1>
-    <div class="flex space-x-10 py-5">
+    <div class="flex flex-col p-10 justify-center items-center">
         <div class="flex flex-col space-y-5">
-            <div class="bottom"><Button color="blue"> Back </Button></div>
             <div class="flex flex-col bg-white outline-1 outline-transparent shadow-md shadow-black rounded-xl space-y-2 p-5">
                 <h2 class="text-xl px-2 font-semibold text-center">Slide 1/7</h2>
                 <Hr /> 
@@ -21,6 +21,5 @@
                 <Textarea bind:value={summaryText} id="textarea-id" rows="5" name="summary" />
             </div>
         </div>
-        <div><Button color="blue"> Complete Summary </Button></div>
     </div>
 </div>
