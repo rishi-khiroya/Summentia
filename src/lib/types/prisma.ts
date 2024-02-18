c805304840a2723038f5f439983335535dfb15d6
Khiroya Rishi <rishi.khiroya21@imperial.ac.uk>
2024-02-18 16:02:55 +0000

    feat: Update prisma schema and add types.

--- src/lib/types/prisma.ts
+++ src/lib/types/prisma.ts
@@ -1,4 +1,5 @@
 export interface PrismaUser {
+	id: string;
 	name: string;
 	email: string;
 }
@@ -6,9 +7,31 @@ export interface PrismaUser {
 export interface PrismaProject {
 	title: string;
 	userId: number;
-	date: Date | undefined;
-	videoUrl: string | undefined;
-	audioUrl: string | undefined;
-	transcript: string | undefined;
-	summary: string | undefined;
+	date: string;
+	createdAt: string;
+	hasSlides: boolean;
+	video: string; // path of the video on the vm
+	data: PrismaBasicData | PrismaSlidesData[];
+	status: PrismaProjectStatus;
+	waiting: boolean;
+}
+
+export interface PrismaSlidesData {
+	slide: string; // path of the slide (as an img) on the vm
+	transcript: string;
+	summary: string;
+}
+
+export interface PrismaBasicData {
+	transcript: string;
+	summary: string;
+}
+
+export enum PrismaProjectStatus {
+	UNPROCESSED,
+	SPLIT,
+	TRANSCRIBED,
+	SUMMARISED,
+	OUTPUT,
+	COMPLETED
 }
