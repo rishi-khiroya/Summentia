6c1696c51181f6f880c8b7ee7a612315cc114a81
BrettConway2 <brett@conwayclan.com>
2024-02-18 22:40:32 +0000

    feat: Add arrow shaped buttons and labels
    Add arrow shaped ellipse buttons and labels. Change page.server.ts temporarily to load slide_editor page immediately for testing.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,6 +5,6 @@ import { redirect } from "@sveltejs/kit";
 export const load: PageServerLoad = async (event) => {
 	const session: Session | null = await event.locals.auth();
     console.log(session?.user);
-	if (session?.user) throw redirect(303, '/dashboard');
+	if (session?.user) throw redirect(303, '/slide_editor');
 	return {};
 }
--- src/routes/slide_editor/+page.svelte
+++ src/routes/slide_editor/+page.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
     import { Badge, ListPlaceholder, TextPlaceholder, Hr, Button, ImagePlaceholder, Textarea  } from 'flowbite-svelte';
-    import { ArrowRightOutline,  ArrowDownToBracketOutline, PlusSolid} from 'flowbite-svelte-icons';
+    import { ArrowRightOutline,  ArrowLeftOutline, ArrowDownToBracketOutline, PlusSolid} from 'flowbite-svelte-icons';
 
-    let summaryText = 'This is the second lecture for the Introduction to Machine Learning course';
+    // bound to text area and slide image.
+    // Will to be set to relevant slide PNG and summary
+    let currentSlideSummary = 'This is the second lecture for the Introduction to Machine Learning course';
+    let currentSlideImage = "placeholder_slide.PNG";
+
+    
+
 </script>
 
     <h1 class="text-5xl p-10 font-bold">Slide Editor</h1>
@@ -14,23 +20,24 @@
 
                 <h2 class="text-xl px-2 font-semibold text-center">Slide 1/7</h2>
                 <Hr /> 
-                <img width="450px" src="placeholder_slide.PNG" alt="Placeholder Slide Image" />
+
+                <!-- svelte-ignore a11y-img-redundant-alt -->
+                <img width="450px" src={currentSlideImage} alt="Placeholder Slide Image" />
                 <Hr /> 
 
             </div>
 
             <div class="flex flex-col bg-white outline-1 outline-transparent shadow-md shadow-black rounded-xl space-y-2 p-5">
                 <h2 class="text-xl px-2 font-semibold">Summary</h2>
-                <Textarea bind:value={summaryText} id="textarea-id" rows="5" name="summary" />
+                <Textarea bind:value={currentSlideSummary} id="textarea-id" rows="5" name="summary" />
             </div>
             
         </div>
     </div>
     
-<div class="fixed bottom-60 left-80 p-5">
-    <Button color="blue" size="sm">
-        &lt Last Slide
-    </Button>
+<div class="fixed bottom-10 left-40 p-40">
+    <Button color="dark" pill={true} class="fixed bottom-60 left-80 p-5"><ArrowLeftOutline class="w-4 h-4" /></Button>
+Previous Slide
 </div>
 
 <div class="fixed bottom-0 left-0 p-5">
@@ -39,14 +46,13 @@
     </Button>
 </div>
 
-<div class="fixed bottom-60 right-80 p-5">
-    <Button color="blue" size="sm">
-        Next Slide >
-    </Button>
+<div class="fixed bottom-10 right-40 p-40">
+    <Button color="dark" pill={true} class="fixed bottom-60 right-80 p-5"><ArrowRightOutline class="w-4 h-4" /></Button>
+Previous Slide
 </div>
 
 <div class="fixed bottom-0 right-0 p-5">
-    <Button color="blue" size="sm">
+    <Button color="blue" size="sm" background-color="transparent">
         Complete Summary
     </Button>
 </div>
