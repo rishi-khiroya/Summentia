ca9210b1fb1282f2c0e512fc4aca463e117f54cd
Gabriel de Olim Gaul <gabdeolimgaul@outlook.com>
2024-02-19 00:59:57 +0000

    feat: Implement load for per-project [slug] page

---
+++ src/routes/projects/[slug]/+page.server.ts
@@ -0,0 +1,27 @@
+import type { Session } from '@auth/core/types';
+import type { PageServerLoad } from '../../$types';
+import { redirect, error } from '@sveltejs/kit';
+import { prisma } from '$lib/prisma';
+import type { Project } from '@prisma/client'
+
+export const load: PageServerLoad = async (event) => {
+    const session: Session | null = await event.locals.auth();
+    if (!session?.user) throw redirect(303, "/");
+
+    const pageNoParam: string | null = event.url.searchParams.get('page');
+    const pageNo: number = pageNoParam ? Number(pageNoParam) : 0;
+
+    const slug = event.params.slug;
+
+    if (isNaN(Number(slug))) error(404, "Error: Project not found.");
+
+    const project: Project | null = await prisma.project.findUnique({
+        where: {
+            id: Number(slug)
+        }
+    });
+
+    if (project == null) error(503, "Unable to fetch project details. Please try again later.");
+
+    return { pageNo, project }
+};
