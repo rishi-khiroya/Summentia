0004bd3a22d1c95793076c8c32e2494aebfdcaea
BrettConway2 <brett@conwayclan.com>
2024-02-20 16:22:48 +0000

    feat: Make slide_editor page multi-slide
    Add every slide and summary in given arrays to the slide editor page. Placeholder images also committed with temporary hard-coded summaries until backend connection

--- src/routes/slide_editor/+page.svelte
+++ src/routes/slide_editor/+page.svelte
@@ -1,55 +1,49 @@
 <script lang="ts">
-    import { Badge, ListPlaceholder, TextPlaceholder, Hr, Button, ImagePlaceholder, Textarea  } from 'flowbite-svelte';
-    import { ArrowRightOutline,  ArrowLeftOutline, ArrowDownToBracketOutline, PlusSolid} from 'flowbite-svelte-icons';
+    import {Hr, Button, Textarea  } from 'flowbite-svelte';
 
-    // bound to text area and slide image.
-    // Will to be set to relevant slide PNG and summary
-    let currentSlideSummary = 'This is the second lecture for the Introduction to Machine Learning course';
-    let currentSlideImage = "placeholder_slide.PNG";
-    let slideNumber = 1;
+    let slides = ["placeholder_slide_1.PNG", "placeholder_slide_2.PNG", "placeholder_slide_3.PNG",
+                  "placeholder_slide_4.PNG", "placeholder_slide_5.PNG","placeholder_slide_6.PNG"];
 
-    
-
+    let summaries = [`This is the second lecture for the Introduction to Machine Learning course`,
+                     `This is the course plan for the Introduction to Machine Learning course`,
+                     `Here are some of the classification algorithms we will learn about`,
+                     `Lazy learner models, like k-nearest neighbors (kNN), don't build a model during training; they store the data and compute predictions based on similarity at prediction time. They're intuitive, handle data changes well, but are computationally expensive and memory-intensive.
+                      Eager learner models, exemplified by decision trees, build a model during training and generalize data patterns. They're faster at prediction, less memory-intensive, but require more computational resources during training and may overfit if not properly regularized.`,
+                     `Today we will be learning about classification with instance based learning versus classification with decision trees`,
+                     `Today's lecture will be delivered in 6 short videos`
+                    ];
 </script>
 
-    <h1 class="text-5xl p-10 font-bold">Slide Editor</h1>
+<h1 class="text-5xl p-10 font-bold">Slide Editor</h1>
 
-    <div class="flex flex-col p-0 justify-center items-center">
-        <div class="flex flex-col space-y-5">
+<div class="flex flex-row flex-wrap p-0 justify-center items-start">
+    {#each slides as slide, index}
+        <div class="flex flex-col space-y-5 m-5">
 
-            <div class="flex flex-col bg-white outline-1 outline-transparent shadow-md shadow-black rounded-xl space-y-2 p-5">
+            <div class="flex flex-col bg-white outline-1 outline-transparent shadow-md shadow-black rounded-xl space-y-2 p-10">
 
-                <h2 class="text-xl px-2 font-semibold text-center">Slide {slideNumber}/7</h2>
+                <h2 class="text-xl px-2 font-semibold text-center">Slide {index + 1}/{slides.length}</h2>
                 <Hr /> 
 
                 <!-- svelte-ignore a11y-img-redundant-alt -->
-                <img width="450px" src={currentSlideImage} alt="Placeholder Slide Image" />
+                <img width="450px" src={slide} alt="Slide {index + 1}" />
                 <Hr /> 
 
             </div>
 
             <div class="flex flex-col bg-white outline-1 outline-transparent shadow-md shadow-black rounded-xl space-y-2 p-5">
-                <h2 class="text-xl px-2 font-semibold">Summary</h2>
-                <Textarea bind:value={currentSlideSummary} id="textarea-id" rows="5" name="summary" />
+                <h2 class="text-xl px-2 font-semibold">Summary {index + 1}/{slides.length}</h2>
+                <Textarea readonly rows="5" cols="50" value={summaries[index]} />
             </div>
             
         </div>
-    </div>
-    
-<div class="fixed bottom-10 left-40 p-40">
-    <Button color="dark" pill={true} class="fixed bottom-60 left-80 p-5"><ArrowLeftOutline class="w-4 h-4" /></Button>
-Previous Slide
+    {/each}
 </div>
 
 <div class="fixed bottom-0 left-0 p-5">
     <Button color="blue" size="sm">
         Back
     </Button>
-</div>
-
-<div class="fixed bottom-10 right-40 p-40">
-    <Button color="dark" pill={true} class="fixed bottom-60 right-80 p-5"><ArrowRightOutline class="w-4 h-4" /></Button>
-Next Slide
 </div>
 
 <div class="fixed bottom-0 right-0 p-5">
