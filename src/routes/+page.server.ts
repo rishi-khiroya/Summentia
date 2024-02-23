536217dcd08fa4294a9e861d77b08e3f9b61f2f1
Khiroya Rishi <rishi.khiroya21@imperial.ac.uk>
2024-02-23 07:17:58 +0000

    feat: Redirect from home page to sign in page if not logged in.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,10 +1,12 @@
 import type { Session } from '@auth/core/types';
 import type { PageServerLoad } from './new/$types';
 import { redirect } from '@sveltejs/kit';
+import { signIn } from '@auth/sveltekit/client';
 
 export const load: PageServerLoad = async (event) => {
 	const session: Session | null = await event.locals.auth();
-	console.log(session?.user);
+	// console.log(session?.user);
 	if (session?.user) throw redirect(303, '/dashboard');
+	else signIn('auth0');
 	return {};
 };
