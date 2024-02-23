aee324752abdbdf9041aea88c0b49002dc14424e
Khiroya Rishi <rishi.khiroya21@imperial.ac.uk>
2024-02-23 07:47:04 +0000

    fix: Remove faulty redirect to sign in

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -7,6 +7,6 @@ export const load: PageServerLoad = async (event) => {
 	const session: Session | null = await event.locals.auth();
 	// console.log(session?.user);
 	if (session?.user) throw redirect(303, '/dashboard');
-	else signIn('auth0');
+	// else signIn('auth0');
 	return {};
 };
