263ad02ef9276a97f0b4285478efec5d071ec265
Khiroya Rishi <rishi.khiroya21@imperial.ac.uk>
2024-02-23 07:20:47 +0000

    feat: Update dashboard.

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -9,9 +9,15 @@
 		TableHead,
 		TableHeadCell,
 		Table,
-		Hr
+		Hr,
+		Tooltip
 	} from 'flowbite-svelte';
-	import { ArrowRightOutline } from 'flowbite-svelte-icons';
+	import {
+		ArrowRightOutline,
+		DownloadOutline,
+		EditOutline,
+		EyeOutline
+	} from 'flowbite-svelte-icons';
 	import { reformat_date } from '$lib/utils.js';
 	import { goto } from '$app/navigation';
 	import { PrismaProjectStatus } from '$lib/types/Prisma';
@@ -30,12 +36,12 @@
 			<h2 class="text-xl px-2 font-semibold dark:text-white">Your most recent summary:</h2>
 			{#if projects?.length}
 				<div class="flex flex-row justify-between space-x-5 p-3">
-					<h1 class="text-4xl">{projects[0].title}</h1>
+					<h1 class="text-2xl">{projects[0].title}</h1>
 					<h3 class="text-xl">{reformat_date(projects[0].date)}</h3>
 				</div>
 				<Hr />
 				<h3>{projects[0].data.summary}</h3>
-				<div class="flex w-full justify-end">
+				<div class="flex w-full justify-end p-5">
 					<Button href={`/projects/${projects[0].id}`}>View</Button>
 				</div>
 			{:else}
@@ -52,23 +58,41 @@
 					<TableHeadCell>Title</TableHeadCell>
 					<TableHeadCell>Date</TableHeadCell>
 					<TableHeadCell>Status</TableHeadCell>
+					<TableHeadCell>Quick Actions</TableHeadCell>
 				</TableHead>
 				<TableBody>
 					{#each projects as item}
-						<TableBodyRow on:click={() => goto(`/projects/${item.id}`)}>
-							<!-- <TableBodyCell>{item.id}</TableBodyCell> -->
+						<TableBodyRow>
 							<TableBodyCell>{item.title}</TableBodyCell>
 							<TableBodyCell>{reformat_date(item.date)}</TableBodyCell>
-							<TableBodyCell
-								>{item.status[0].toUpperCase() +
-									item.status.substring(1).toLowerCase()}</TableBodyCell
-							>
+							<TableBodyCell>
+								{item.status[0].toUpperCase() + item.status.substring(1).toLowerCase()}
+							</TableBodyCell>
+							<TableBodyCell>
+								<div class="flex flex-row space-x-5">
+									<button
+										class="hover:cursor-pointer"
+										on:click={() => goto(`/projects/${item.id}`)}
+									>
+										<EyeOutline size="lg" id="view" />
+										<Tooltip triggeredBy="#view">View Project</Tooltip>
+									</button>
+									<button class="hover:cursor-pointer" on:click={() => goto(`/edit/${item.id}`)}>
+										<EditOutline size="lg" id="edit" />
+										<Tooltip triggeredBy="#edit">Edit Project</Tooltip>
+									</button>
+									<button class="hover:cursor-pointer">
+										<DownloadOutline size="lg" id="download" />
+										<Tooltip triggeredBy="#download">Download Project</Tooltip>
+									</button>
+								</div>
+							</TableBodyCell>
 						</TableBodyRow>
 					{/each}
 				</TableBody>
 			</Table>
 			<div class="flex w-full mt-10 justify-end align-bottom">
-				<Button outline color="alternative" href="/prijects">
+				<Button outline color="alternative" href="/projects">
 					All Projects <ArrowRightOutline class="w-3.5 h-3.5 ms-2" />
 				</Button>
 			</div>
