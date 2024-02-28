63e646614ec706bbec43f30e2fb9578228c31a28
Khiroya Rishi <rishi.khiroya21@imperial.ac.uk>
2024-02-28 10:46:22 +0000

    fix: Modify inprogress page to call a fetch action to refresh data instead of invalidateAll()

--- src/routes/new/inprogress/[slug]/+page.server.ts
+++ src/routes/new/inprogress/[slug]/+page.server.ts
@@ -30,88 +30,115 @@ export const load: PageServerLoad = async ({ params }) => {
             redirect(302, `/edit/${data.id}`);
         }
     }
+    return { id: data.id, status: PrismaProjectStatus[data.status], waiting: data.waiting };
 
-    const project: PrismaProject = {
-        id: data.id,
-        title: data.title,
-        userId: data.userId,
-        date: data.date,
-        createdAt: data.createdAt,
-        hasSlides: data.hasSlides,
-        video: data.video, // path of the video on the vm
-        slides: data.slides,
-        data: JSON.parse(JSON.stringify(data.data)),
-        status: PrismaProjectStatus[data.status],
-        waiting: data.waiting,
-        customisation: JSON.parse(JSON.stringify(data.customisation))
-    }
+}
 
+export const actions = {
+    fetch: async ({ params }) => {
 
-    if (project.waiting) {
-        if (project.status == PrismaProjectStatus.UNPROCESSED) {
-            console.log("Sending request to Python back-end to complete splitting.");
+        const data = await prisma.project.findUnique({
+            where: {
+                id: Number(params.slug)
+            }
+        });
 
+        if (!data) error(503, "Project not found.");
+
+        if (!data.waiting) {
+            if (data.status == 'COMPLETED') {
+                redirect(303, `/projects/${data.id}`);
+            } else if (data.status == 'SUMMARISED') {
+                redirect(303, `/edit/${data.id}`);
+            }
         }
 
-        if (project.status == PrismaProjectStatus.SPLIT) {
+        const project: PrismaProject = {
+            id: data.id,
+            title: data.title,
+            userId: data.userId,
+            date: data.date,
+            createdAt: data.createdAt,
+            hasSlides: data.hasSlides,
+            video: data.video, // path of the video on the vm
+            slides: data.slides,
+            data: JSON.parse(JSON.stringify(data.data)),
+            status: PrismaProjectStatus[data.status],
+            waiting: data.waiting,
+            customisation: JSON.parse(JSON.stringify(data.customisation))
+        }
 
-            console.log("Starting transcription.")
-            await setWaiting(project.id);
+        let record;
 
-            const extlessPath: string = project.video.substring(0, project.video.lastIndexOf('.'));
+        if (project.waiting) {
+            if (project.status == PrismaProjectStatus.UNPROCESSED) {
+                console.log("Sending request to Python back-end to complete splitting.");
 
-            let data;
-            if (project.hasSlides) {
-                data = [{ slide: "Not implemented yet.", transcript: "Not implemented yet.", summary: "Not implemented yet." }] as PrismaSlidesData[];
-            } else {
-                const transcript = await transcribe(extlessPath);
-                data = { transcript: transcript ? transcript : "Unable to transcribe.", summary: "" } as PrismaBasicData;
             }
 
-            await prisma.project.update({
-                where: { id: project.id },
-                data: {
-                    // @ts-expect-error: Cannot force the typecast.
-                    data,
-                    waiting: true,
-                    status: 'TRANSCRIBED'
+            if (project.status == PrismaProjectStatus.SPLIT) {
+
+                console.log("Starting transcription.")
+                await setWaiting(project.id);
+
+                const extlessPath: string = project.video.substring(0, project.video.lastIndexOf('.'));
+
+                let data;
+                if (project.hasSlides) {
+                    data = [{ slide: "Not implemented yet.", transcript: "Not implemented yet.", summary: "Not implemented yet." }] as PrismaSlidesData[];
+                } else {
+                    const transcript = await transcribe(extlessPath);
+                    data = { transcript: transcript ? transcript : "Unable to transcribe.", summary: "" } as PrismaBasicData;
                 }
-            })
-            console.log("Finished transcription.")
-        }
 
-    }
-
-    if (project.status == PrismaProjectStatus.TRANSCRIBED) {
-
-        console.log("Starting summarisation.");
-        await setWaiting(project.id);
-
-        let data;
-        if (project.hasSlides) {
-            const slidesData: PrismaSlidesData[] = project.data as PrismaSlidesData[];
-            slidesData.forEach(async slideData => {
-                const summary: string | null = await summarise(slideData.transcript, project.customisation.summaryLevel - 1);
-                slideData.summary = summary ? summary : "Error...";
-            })
-            data = slidesData.map(data => JSON.stringify(data));
-        } else {
-            const transcript: string = (project.data as PrismaBasicData).transcript;
-            const summary: string | null = await summarise(transcript);
-            data = { transcript, summary: summary ? summary : "Error..." };
-        }
-
-        await prisma.project.update({
-            where: { id: project.id },
-            data: {
-                data,
-                waiting: false,
-                status: 'SUMMARISED'
+                record = await prisma.project.update({
+                    where: { id: project.id },
+                    data: {
+                        // @ts-expect-error: Cannot force the typecast.
+                        data,
+                        waiting: true,
+                        status: 'TRANSCRIBED'
+                    }
+                })
+                console.log("Finished transcription.")
             }
-        })
-        console.log("Finished summarisation.");
-    }
 
-    return { project };
+            if (project.status == PrismaProjectStatus.TRANSCRIBED) {
+
+                console.log("Starting summarisation.");
+                await setWaiting(project.id);
+
+                let data;
+                if (project.hasSlides) {
+                    const slidesData: PrismaSlidesData[] = project.data as PrismaSlidesData[];
+                    slidesData.forEach(async slideData => {
+                        const summary: string | null = await summarise(slideData.transcript, project.customisation.summaryLevel - 1);
+                        slideData.summary = summary ? summary : "Error...";
+                    })
+                    data = slidesData.map(data => JSON.stringify(data));
+                } else {
+                    const transcript: string = (project.data as PrismaBasicData).transcript;
+                    const summary: string | null = await summarise(transcript);
+                    data = { transcript, summary: summary ? summary : "Error..." };
+                }
+
+                record = await prisma.project.update({
+                    where: { id: project.id },
+                    data: {
+                        data,
+                        waiting: false,
+                        status: 'SUMMARISED'
+                    }
+                })
+                console.log("Finished summarisation.");
+            }
+
+        }
+
+        return {
+            project: JSON.stringify(record)
+        };
+
+    }
 
 }
--- src/routes/new/inprogress/[slug]/+page.svelte
+++ src/routes/new/inprogress/[slug]/+page.svelte
@@ -1,20 +1,55 @@
 <script lang="ts">
 	import { invalidateAll } from '$app/navigation';
-	import { PrismaProjectStatus } from '$lib/types/Prisma.js';
+	import { PrismaProjectStatus, type PrismaProject } from '$lib/types/Prisma.js';
+	import { formResponseToJSON } from '$lib/utils.js';
 	import { Spinner } from 'flowbite-svelte';
 	import { onDestroy } from 'svelte';
 
 	export let data;
 
+	let project;
+	let fetched: boolean = false;
+
+	if (data?.status) fetched = true;
+	console.log(data);
+
+	let fetching: boolean = false;
+
 	const interval = setInterval(async () => {
+		// invalidateAll();
+
+		if (fetching) return;
 		console.log('Refreshing data...');
-		invalidateAll();
+		fetching = false;
+
+		const form = new FormData();
+		form.append('id', data.id.toString());
+
+		const response = await fetch('?/fetch', {
+			method: 'POST',
+			body: form
+		});
+
+		if (response.ok) {
+			console.log(response);
+			const json = await response.json();
+			console.log(json);
+			const responseData = formResponseToJSON(json.project.toString());
+
+			console.log(responseData);
+			project = responseData;
+			fetched = true;
+		}
+
+		fetching = false;
+
+
 	}, 3000);
 
 	onDestroy(() => clearInterval(interval));
 
 	const status: () => string = (): string => {
-		switch (data.project.status) {
+		switch (data.status) {
 			case PrismaProjectStatus.SPLIT:
 				return 'Transcribing...';
 			case PrismaProjectStatus.TRANSCRIBED:
@@ -28,10 +63,12 @@
 </script>
 
 <div class="flex flex-col w-full p-10 m-20 bg-white dark:bg-slate-800 shadow-xl rounded-2xl">
-	{#key data}
+	{#key project}
 		<div class="flex-col text-center p-10 space-y-5">
 			<Spinner size="16" />
-			<h1 class="text-4xl animate-bounce pt-3 dark:text-white">{status()}</h1>
+			{#if fetched}
+				<h1 class="text-4xl animate-bounce pt-3 dark:text-white">{status()}</h1>
+			{/if}
 		</div>
 	{/key}
 </div>
