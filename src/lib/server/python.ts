f448690301554adc7c6fce5959a3cb3b38cb3a07
Rishi Khiroya <rk1121@ic.ac.uk>
2024-03-06 10:22:37 +0000

    feat: Implement python calls to back-end

--- src/lib/server/python.ts
+++ src/lib/server/python.ts
@@ -1,4 +1,4 @@
-const PYTHON_URL = 'http://localhost:8000';
+import { PYTHON_URL } from '$env/static/private';
 
 export async function process_noslides(project_folder: string): Promise<string> {
 	const response = await fetch(`${PYTHON_URL}/process_noslides`, {
@@ -8,4 +8,24 @@ export async function process_noslides(project_folder: string): Promise<string> 
 	});
 	const transcript = await response.text();
 	return transcript;
+}
+
+export async function process_slides(project_folder: string) {
+	const response = await fetch(`${PYTHON_URL}/process_slides`, {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({ project_folder })
+	});
+	const data = await response.json();
+	return data;
+}
+
+export async function process_genslides(project_folder: string) {
+	const response = await fetch(`${PYTHON_URL}/process_genslides`, {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify(project_folder)
+	});
+	const data = await response.json();
+	return data;
 }
