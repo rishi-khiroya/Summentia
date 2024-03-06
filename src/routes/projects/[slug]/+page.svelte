c0f569c5433818017b734b2fab5c8740ebcf3bc7
bjorna <bjornapermeti@gmail.com>
2024-03-06 19:48:02 +0000

    feat: Started flashcards

--- src/routes/projects/[slug]/+page.svelte
+++ src/routes/projects/[slug]/+page.svelte
@@ -5,12 +5,15 @@
 		EditSolid,
 		DownloadSolid,
 		ChevronLeftOutline,
-		ChevronRightOutline
+		ChevronRightOutline,
+		CreditCardOutline
 	} from 'flowbite-svelte-icons';
 	import { ButtonGroup, Button, Pagination, Video } from 'flowbite-svelte';
 	import type { PrismaBasicData, PrismaSlidesData } from '$lib/types/prisma';
 	import DownloadModal from '../../DownloadModal.svelte';
 	import type { Customisation } from '$lib/types/Customisation';
+	import { formResponseToJSON } from '$lib/utils';
+
 
 	const accumulateSlideData = (slideData: PrismaSlidesData[]): PrismaBasicData => {
 		let basicData: PrismaBasicData;
@@ -67,6 +70,31 @@
 	const changeView = () => {
 		displaySlides = !displaySlides;
 	};
+	
+	const handleFlashCards = async () => {
+		console.log("sfnewisfjew");
+		let summary = "";
+		if(data.project.hasSlides){
+			slideData.forEach((item) => item.summaries.forEach((summary_object) => {summary = summary + " " + summary_object}));
+		} else {
+			summary = overviewData.summary
+		}
+		
+		const form = new FormData();
+		form.append('summary', summary);
+
+		const response = await fetch('?/flashCards', {
+			method: 'POST',
+			body: form
+		})
+		if (response.ok) {
+			const json = await response.json();
+			const responseData = formResponseToJSON(json.flashCards.toString());
+
+			console.log("e morem ktu: " + responseData);
+		}
+
+	};
 
 	const previous = () => {
 		const params = new URLSearchParams(window.location.search);
@@ -94,6 +122,8 @@
 	function edit() {
 		goto(`/edit/${data.project.id}`);
 	}
+
+	
 </script>
 
 <!--  the filename will be stored in the S3 storage with the format title_id -->
@@ -112,6 +142,10 @@
 					<Button pill color="dark" on:click={() => (showDownloadModal = true)}>
 						<DownloadSolid class="me-2 focus:!outline-none" />
 						Download
+					</Button>
+					<Button pill color="dark" on:click{handleFlashCards}>
+						<CreditCardOutline class="me-2 focus:!outline-none"/>
+						Generate Flash Cards
 					</Button>
 				</ButtonGroup>
 			</div>
@@ -132,7 +166,9 @@
 						/>
 					</div>
 					<InformationBox title="Transcript:" maxHeight="72" additionalAttributes="flex-1">
-						<p>{slideData[slideNo].transcript}</p>
+						<p>{
+							slideData[slideNo].transcripts.reduce((a, b) => a + " " + b, "")
+							}</p>
 					</InformationBox>
 				</div>
 				<div class="flex">
@@ -144,7 +180,9 @@
 						/>
 					</div>
 					<InformationBox title="Summary:" maxHeight="72" additionalAttributes="flex-1">
-						<p>{slideData[slideNo].summary}</p>
+						<p>{
+							slideData[slideNo].summaries.reduce((a, b) => a + " " + b, "")
+						}</p>
 					</InformationBox>
 				</div>
 			</div>
