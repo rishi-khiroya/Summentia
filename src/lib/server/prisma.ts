cd688e8b0a9ef04dc9ac06404ce569270e83d58c
Rishi Khiroya <rk1121@ic.ac.uk>
2024-03-08 17:35:31 +0000

    fix: Fix node build.

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,19 +1,5 @@
-// import { PrismaClient } from '@prisma/client';
-
-// const prisma = new PrismaClient();
-
-// export default prisma;
-
-import type { PrismaClient as ImportedPrismaClient } from '@prisma/client';
-import { createRequire } from 'module';
-
-const require = createRequire(import.meta.url);
-
-const { PrismaClient: RequiredPrismaClient } = require('@require/client');
-
-const _PrismaClient: typeof ImportedPrismaClient = RequiredPrismaClient;
-
-export class PrismaClient extends _PrismaClient {}
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
+
 export default prisma;
