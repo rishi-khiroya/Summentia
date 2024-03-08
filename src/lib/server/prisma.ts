e49cb309ed17093e4a1690ac348ddf5d02237f95
Rishi Khiroya <rk1121@ic.ac.uk>
2024-03-08 13:23:34 +0000

    fix: Try a prisma svelte fix

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,5 +1,19 @@
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
+
+// const prisma = new PrismaClient();
+
+// export default prisma;
+
+import type { PrismaClient as ImportedPrismaClient } from '@prisma/client';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
+const { PrismaClient: RequiredPrismaClient } = require('@require/client');
+
+const _PrismaClient: typeof ImportedPrismaClient = RequiredPrismaClient;
+
+export class PrismaClient extends _PrismaClient {}
 
 const prisma = new PrismaClient();
-
 export default prisma;
