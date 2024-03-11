9eaf092bca6f954888b5cce9c629516bf00117d9
Rishi Khiroya <rk1121@ic.ac.uk>
2024-03-11 11:30:09 +0000

    fix: Modify save button on edit page.

--- src/routes/edit/[slug]/+page.svelte
+++ src/routes/edit/[slug]/+page.svelte
@@ -98,8 +98,11 @@
 			body: form
 		});
 
-		if (response.ok) saved = true;
-		alert("The change is saved")
+		if (response.ok) {
+			saved = true;
+			dirty = false;
+		}
+		alert('The change is saved');
 		// TODO: trigger toast
 	}
 
@@ -214,6 +217,10 @@
 								cols="100"
 								bind:value={slideData.summaries}
 								on:change={() => (saved = false)}
+								on:input={() => {
+									saved = false;
+									dirty = true;
+								}}
 							/>
 						</div>
 					</div>
@@ -238,8 +245,10 @@
 <div class="fixed bottom-0 left-0 p-5">
 	<Button color="red" size="xl" on:click={() => back()}>Back</Button>
 
-	<Button color="green" size="xl" on:click={() => save()}>
-		Save
-		<FileCheckSolid />
-	</Button>
+	{#if !saved || dirty}
+		<Button color="green" size="xl" on:click={() => save()}>
+			Save
+			<FileCheckSolid />
+		</Button>
+	{/if}
 </div>
